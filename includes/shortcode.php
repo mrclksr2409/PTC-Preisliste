8869d30e14cb5e1947877f986cb6d1ce6deb6725
mrclksr2409 <51036118+mrclksr2409@users.noreply.github.com>
2020-11-27 12:35:01 +0100

    Shortcode

--- includes/shortcode.php
+++ includes/shortcode.php
@@ -18,10 +18,19 @@ function create_preisliste_shortcode($atts) {
   $post_fields = get_fields( $id );
 
   if($field == "title") {
-    get_the_title( $id );
+    echo get_the_title( $id );
   }
   elseif ($field == "beschreibung") {
-    echo $post_fields['beschreibung'];
+    echo $post_fields[ $field ];
+  }
+  elseif ($field == "gruppe") {
+    echo $post_fields[ $field ];
+  }
+  elseif ($field == "12_monate" || $field == "24_monate" || $field == "36_monate" || $field == "48_monate" || $field == "60_monate") {
+    echo $post_fields['abo'][ $field ];
+  }
+  elseif ($field == "monatlich" || $field == "einmalig") {
+    echo $post_fields['kaufen'][ $field ];
   }
 
 }
