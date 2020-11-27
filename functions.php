fea100d1e0a2402e1fa499f0a7aaad8d1b182dfc
mrclksr2409 <51036118+mrclksr2409@users.noreply.github.com>
2020-11-27 10:41:20 +0100

    0.3.2

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: PTC Preisliste
 Description: none
-Version: 0.3.1
+Version: 0.3.2
 Author: Marcel Kaiser
 Author URI: https://www.ptc-telematik.de/
 */
@@ -18,12 +18,6 @@ $myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
  * Post Type
  */
 include_once('includes/post_type.php');
-
-/**
- * Taxonomy
- */
-
-include_once('includes/taxonomy.php');
 
 /**
  * Advanced Custom Fields
