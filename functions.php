daa91f1b64907367b9b93ec8fd05506e5a03ec3d
mrclksr2409 <51036118+mrclksr2409@users.noreply.github.com>
2020-11-27 11:10:05 +0100

    036

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: PTC Preisliste
 Description: none
-Version: 0.3.5
+Version: 0.3.6
 Author: Marcel Kaiser
 Author URI: https://www.ptc-telematik.de/
 */
@@ -13,6 +13,29 @@ $myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
 	__FILE__,
 	'PTCpreisliste'
 );
+
+/**
+ * Advanced Custom Fields Plugin
+ */
+
+// Define path and URL to the ACF plugin.
+define( 'MY_ACF_PATH', get_stylesheet_directory() . '/includes/acf/' );
+define( 'MY_ACF_URL', get_stylesheet_directory_uri() . '/includes/acf/' );
+
+// Include the ACF plugin.
+include_once( MY_ACF_PATH . 'acf.php' );
+
+// Customize the url setting to fix incorrect asset URLs.
+add_filter('acf/settings/url', 'my_acf_settings_url');
+function my_acf_settings_url( $url ) {
+    return MY_ACF_URL;
+}
+
+// (Optional) Hide the ACF admin menu item.
+add_filter('acf/settings/show_admin', 'my_acf_settings_show_admin');
+function my_acf_settings_show_admin( $show_admin ) {
+    return false;
+}
 
 /**
  * Post Type
